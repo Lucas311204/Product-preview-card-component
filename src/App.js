d182df027c7dc8cd569589deb9e17154acd4e555
Lucas311204 <82537351+Lucas311204@users.noreply.github.com>
2023-02-21 14:43:59 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Produto from "./product-image.jpg";
+import "./App.css";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="Card">
+        <div className="Imagem">
+          <img src={Produto} alt="produto"></img>
+        </div>
+
+        <div id="conteudo">
+          <h3 id="titulo">P E R F U M E</h3>
+
+          <h1>Gabrielle Essence Eau de Parfum</h1>
+          <p class="texto">
+            A floral, solar and voluptuous interpretation composed by Oliver
+            Polge, Perfumer-Creator for the house of CHANEL
+          </p>
+          <div class="valores">
+            <h1 id="preco1">$149.99</h1>
+            <h3 id="preco2">$169.99</h3>
+          </div>
+          <button>
+            <AiOutlineShoppingCart />
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
